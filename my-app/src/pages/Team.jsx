c5c072dddf2d58b7ea7d216826c2a78ffa576cc9
Jixiang Fan <vandejixiang@gmail.com>
2022-08-28 19:46:56 -0400

    update all team

--- my-app/src/pages/Team.jsx
+++ my-app/src/pages/Team.jsx
@@ -1,32 +1,90 @@
-import React from 'react'
-import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Selection, Filter, Page, ExcelExport, PdfExport, Edit, Inject,Toolbar,Search } from '@syncfusion/ej2-react-grids';
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Selection,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+  Toolbar,
+} from "@syncfusion/ej2-react-grids";
 
-import { groupData, contextMenuItems, groupsGrid } from '../data/dummy';
-import { Header } from '../components';
+import { employeesData, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
+import { getDatabase, ref, child, get } from "firebase/database";
+import { getAuth } from "firebase/auth";
 
-const Team = () => {
+const toolbarOptions = [`PdfExport`, `ExcelExport`, `CsvExport`, "Search"];
 
-  const editing = { allowDeleting: true, allowEditing: true };
-  const toolbarOptions = [`Edit`, `Delete`, `Cancel`, `Copy`, 'Search'];
+class Team extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      team_result: [],
+    };
+  }
 
-  return (
-    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-        <Header category="Page" title="Team Lists" />
+  componentDidMount() {
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `team/`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          //console.log(snapshot.val());
+          const team_arr = Object.entries(snapshot.val()).map(
+            ([key, value]) => {
+              delete value["team_member"];
+              delete value["contribution_percentage"];
+              return value;
+            }
+          );
+          console.log(team_arr);
+          this.setState({
+            team_result: team_arr,
+          });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+        <Header category="Page" title="All Team" />
+
         <GridComponent
-        id="gridcomp"
-        dataSource={groupData}
-        allowPaging
-        allowSorting
-        editSettings={editing}
-        toolbar={toolbarOptions}
+          width="auto"
+          dataSource={this.state.team_result}
+          pageSettings={{ pageCount: 5 }}
+          allowPaging
+          allowSorting
+          allowExcelExport
+          allowPdfExport
+          toolbar={toolbarOptions}
         >
-          <ColumnsDirective>
-            {groupsGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
-          </ColumnsDirective>
-          <Inject services={[Page , Toolbar, Edit, Sort, Filter, Search, Selection]} />
+          <Inject
+            services={[
+              Resize,
+              Sort,
+              ContextMenu,
+              Filter,
+              Page,
+              ExcelExport,
+              PdfExport,
+              Toolbar,
+            ]}
+          />
         </GridComponent>
-    </div>
-  )
+      </div>
+    );
+  }
 }
-
-export default Team
+export default Team;
