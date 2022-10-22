0796775e2a9e91e5160587dec5cf31d8e312d914
Jixiang Fan <vandejixiang@gmail.com>
2022-10-22 15:56:34 -0400

    push

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -3,8 +3,7 @@ module.exports = {
     darkMode: 'class',
     theme: {
       screens: {
-        xs: { max: '575px' }, // Mobile (iPhone 3 - iPhone XS Max).
-        sm: { min: '576px', max: '897px' }, // Mobile (matches max: iPhone 11 Pro Max landscape @ 896px).
+        sm: { min: '0px', max: '897px' }, // Mobile 
         md: { min: '898px', max: '1199px' }, // Tablet (matches max: iPad Pro @ 1112px).
         lg: { min: '1200px' }, // Desktop smallest.
         xl: { min: '1159px' }, // Desktop wide.
