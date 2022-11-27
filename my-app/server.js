4e9dee32abd4da3242a461e84774d10dcef80bb7
Jixiang Fan <vandejixiang@gmail.com>
2022-11-27 13:31:01 -0500

    server 0.1 unfinished

--- my-app/server.js
+++ my-app/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+//const routesHandler = require('./routes/handler.js');
+require('dotenv/config')
+
+//app.use('/', routesHandler);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -9,4 +14,15 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(PORT);
+//DB Collection
+mongoose.connect(process.env.DB_URI, {useNewUrlParser:true, useUnifiedTopology:true})
+.then( () => {
+  console.log('DB Connected!')
+})
+.catch( (err) => {
+  console.log(err)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+});
