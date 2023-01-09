68a2d0d5f1e2febb04359bdfadf11cea14fdd3e8
Yusheng Cao <cyschris@vt.edu>
2023-01-09 14:32:51 -0500

    update

--- Backend/routes/handler.js
+++ Backend/routes/handler.js
@@ -1,6 +1,6 @@
 var express = require('express');
-const { Users, Teams,ProgramDate } = require('../models/Schemas.js');
-
+const { Users, Teams, ProgramDate, PersonalExercise, SelfReportActivity, SelfReport, Ranking } = require('../models/Schemas.js');
+const axios = require('axios');
 var router = express.Router();
 const Schemas = require('../models/Schemas.js');
 
@@ -17,10 +17,10 @@ router.get('/profile/:UID', async (req, res) => {
 
 
 router.get("/teams", async (req, res) => {
-  Teams.find().exec(function (err, txs) {
-    //console.log(txs);
-    res.json(txs);
-  });
+    Teams.find().exec(function (err, txs) {
+        //console.log(txs);
+        res.json(txs);
+    });
 });
 
 router.get('/', function (req, res, next) {
@@ -30,5 +30,27 @@ router.get('/', function (req, res, next) {
 router.get('/another/route', function (req, res, next) {
     res.json({ hello: 'world' });
 });
+
+router.get('/serverTestMode', async (req, res) => {
+    //delete all document
+    ProgramDate.deleteMany({}).exec()
+    Users.deleteMany({}).exec()
+    Teams.deleteMany({}).exec()
+    PersonalExercise.deleteMany({}).exec()
+    SelfReportActivity.deleteMany({}).exec()
+    SelfReport.deleteMany({}).exec()
+    Ranking.deleteMany({}).exec()
+    //create Four leader
+
+    res.send("done")
+})
+
+function createLeaderUser(num) {
+    return axios.post("http://localhost:5000/register/AddUserWithoutTeam", {
+        email: email,
+        password: password
+    });
+}
+
 
 module.exports = router;
